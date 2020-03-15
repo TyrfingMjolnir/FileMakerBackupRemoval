60c1288d3cebba89aefc77d803430bbd9e30c734
Tyrfing Mjølner <4lphamal3@gmail.com>
2020-03-15 14:48:23 +0100

    Update FileMakerDeleteOfflineBackup.sh

--- FileMakerDeleteOfflineBackup.sh
+++ FileMakerDeleteOfflineBackup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # Written by Gjermund Gusland Thorsen 1998 - Published in 2020; after upgrade from expr to $(())
+
+#Keep instances of
+keepHourly=23
+keepDaily=5
+#keepWeekly not implemented, as you will most likely keep the last week of each month until drive is full.
+
 cd /Volumes/backup/
 for P in FM18S00; do
     cd $P
@@ -12,7 +18,7 @@ for P in FM18S00; do
     echo "Hourly"
     hc=0
     for D in Hourly_*; do
-        if [ $hc -gt $(( $h - 24 - 1 )) ] # Keep latter 24 hours of the Hourly_* backups
+        if [ $hc -gt $(( $h - $keepHourly - 1 )) ] # Keep latter 24 hours of the Hourly_* backups
         then
             echo "Keep $hc $D"
         else
@@ -25,7 +31,7 @@ for P in FM18S00; do
     echo "Daily"
     dc=0
     for D in Daily_*; do
-        if [ $dc -gt $(( $d - 5 - 1 )) ] # Keep latter 5 days of Daily_* backups
+        if [ $dc -gt $(( $d - $keepDaily - 1 )) ] # Keep latter 5 days of Daily_* backups
         then
             echo "Keep $dc $D"
         else
