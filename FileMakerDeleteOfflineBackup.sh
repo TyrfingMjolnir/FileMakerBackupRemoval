475fc878438b5b1011314f504618e3c2ba98c4b3
Tyrfing Mjølner <4lphamal3@gmail.com>
2020-03-15 14:53:44 +0100

    Update FileMakerDeleteOfflineBackup.sh

--- FileMakerDeleteOfflineBackup.sh
+++ FileMakerDeleteOfflineBackup.sh
@@ -4,7 +4,7 @@
 #Keep instances of
 keepHourly=23
 keepDaily=5
-#keepWeekly not implemented, as you will most likely keep the last week of each month until drive is full.
+keepWeekly=5 implemented on !gh, as a copy of Daily.
 
 cd /Volumes/backup/
 for P in FM18S00; do
@@ -12,33 +12,49 @@ for P in FM18S00; do
     h=$( ls -1t | grep Hourly_ | wc -l )
     d=$( ls -1t | grep Daily_  | wc -l )
     w=$( ls -1t | grep Weekly_ | wc -l )
-    echo "Hourly $h"
-    echo "Daily  $h"
-    echo "Weekly $w"
-    echo "Hourly"
+#    echo "Hourly $h"
+#    echo "Daily  $h"
+#    echo "Weekly $w"
+#    echo "Keep Hourly $keepHourly"
+#    echo "Keep Daily  $keepDaily"
+#    echo "Keep Weekly $keepWeekly"
+#    echo "Hourly"
     hc=0
     for D in Hourly_*; do
         if [ $hc -gt $(( $h - $keepHourly - 1 )) ] # Keep latter $keepHourly hours of the Hourly_* backups
         then
-            echo "Keep $hc $D"
+#            echo "Keep $hc $D"
         else
-            echo "Remove $hc $D"
+#            echo "Remove $hc $D"
             rm -fr $D
         fi
         hc=$(($hc + 1))
     done
 
-    echo "Daily"
+#    echo "Daily"
     dc=0
     for D in Daily_*; do
         if [ $dc -gt $(( $d - $keepDaily - 1 )) ] # Keep latter $keepDaily days of Daily_* backups
         then
-            echo "Keep $dc $D"
+#            echo "Keep $dc $D"
         else
-            echo "Remove $dc $D"
+#            echo "Remove $dc $D"
             rm -fr $D
         fi
         dc=$(($dc + 1))
+    done
+
+#    echo "Weekly"
+    wc=0
+    for D in Weekly_*; do
+        if [ $wc -gt $(( $w - $keepWeekly - 1 )) ] # Keep latter $keepWeekly days of Weekly_* backups
+        then
+#            echo "Keep $wc $D"
+        else
+#            echo "Remove $wc $D"
+            rm -fr $D
+        fi
+        wc=$(($wc + 1))
     done
 
     cd ..
